bbd732ad34489e171494334181f25939ecfda64b
Ryan Bolyard <tiger658@gmail.com>
2020-03-18 17:21:14 -0400

    add script and dependencies

---
+++ delete_runners.py
@@ -0,0 +1,27 @@
+import requests
+import os
+import gitlab
+
+# UPDATE THESE FOR YOUR CONFIGURATION
+gitlab_url = 'https://gitlab.com'
+
+token = os.environ['GITLAB_TOKEN']
+
+kill_string = 'my-runner'
+
+# LOGIC
+
+gl = gitlab.Gitlab(gitlab_url, private_token=token)
+
+runners = gl.runners.list()
+
+print()
+for runner in runners:
+    print("Checking " + str(runner.id) + " - " + runner.description)
+    if runner.description == kill_string:
+        run = gl.runners.get(runner.id)
+        run.delete()
+        print("Deleted " + str(runner.id))
+    else:
+        print("Not Deleting " + str(runner.id) + " - " + runner.description)
+    print()
